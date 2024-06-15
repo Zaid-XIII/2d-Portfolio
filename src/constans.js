14b9c7894b1f7eb5ef12acf437b7ce1d4cb235d1
Zaid-XIII <92167343+Zaid-XIII@users.noreply.github.com>
2024-06-16 00:14:07 +0300

    Update constans.js

--- src/constans.js
+++ src/constans.js
@@ -9,7 +9,7 @@ export const dialogueData = {
      <a href="https://www.youtube.com/@NetworkChuck" target="_blank">NetworkChuck</a>,
     <a href="https://www.youtube.com/@JSLegendDev" target="_blank">JSLegendDev</a> (sometimes) and <a href="https://www.youtube.com/@SimonHoibergExplains" target="_blank">Simon Høiberg</a>!`,
     bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-    resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
+    resume: `This is my desk and on it is my resume. <a href="https://aquamarine-marja-64.tiiny.site/.pdf" target="_blank">Check it out?</a>
     Contact me at zaidalyawer22@gmai.com if you have any interesting job opportunities!`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
